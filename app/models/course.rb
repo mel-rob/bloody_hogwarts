c1254f5a48d7aa7355e304cfeb70728e2ed2654c
Melissa Robbins <melissamrobbins@gmail.com>
2019-12-06 10:37:36 -0700

    Add count method to Course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,8 @@ class Course <ApplicationRecord
   validates_presence_of :name
   has_many :student_courses
   has_many :students, through: :student_courses
+
+  def count
+    students.count
+  end
 end
