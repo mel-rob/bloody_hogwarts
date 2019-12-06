c2be27a12c47adaec12099802c0b2baed76c8747
Melissa Robbins <melissamrobbins@gmail.com>
2019-12-06 10:38:43 -0700

    Add model test for count method in Course model

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -11,4 +11,26 @@ RSpec.describe Course, type: :model do
     it {should have_many(:students).through(:student_courses)}
   end
 
+  it "can count students in class" do
+    @student_1 = Student.create(name: "Harry Potter",
+                                age: 12,
+                                house: "Gryffindor")
+
+    @student_2 = Student.create(name: "Ron Weasley",
+                                age: 11,
+                                house: "Gryffindor")
+
+    @course_1 = Course.create(name: "Defense against the Dark Arts")
+    @course_2 = Course.create(name: "Herbology")
+    @course_3 = Course.create(name: "Potions")
+
+    @student_course_1 = StudentCourse.create(student_id: @student_1.id, course_id: @course_1.id)
+    @student_course_2 = StudentCourse.create(student_id: @student_1.id, course_id: @course_2.id)
+    @student_course_3 = StudentCourse.create(student_id: @student_1.id, course_id: @course_3.id)
+
+    @student_course_4 = StudentCourse.create(student_id: @student_2.id, course_id: @course_1.id)
+    @student_course_5 = StudentCourse.create(student_id: @student_2.id, course_id: @course_2.id)
+
+    expect(@course_1.count).to eq(2)
+  end
 end
