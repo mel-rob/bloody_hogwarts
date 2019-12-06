af249c277710af4e11016b607fe8678ddfbef506
Melissa Robbins <melissamrobbins@gmail.com>
2019-12-06 10:25:30 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+@student_1 = Student.create(name: "Harry Potter",
+                            age: 12,
+                            house: "Gryffindor")
+
+@student_2 = Student.create(name: "Ron Weasley",
+                            age: 11,
+                            house: "Gryffindor")
+
+@course_1 = Course.create(name: "Defense against the Dark Arts")
+@course_2 = Course.create(name: "Herbology")
+@course_3 = Course.create(name: "Potions")
+
+@student_course_1 = StudentCourse.create(student_id: @student_1.id, course_id: @course_1.id)
+@student_course_2 = StudentCourse.create(student_id: @student_1.id, course_id: @course_2.id)
+@student_course_3 = StudentCourse.create(student_id: @student_1.id, course_id: @course_3.id)
